2dfd60e9c6db7e965e3d8c814a7cfee105309567
kibrom hailemariam <kibromhailemariam21@gmail.com>
2025-08-01 19:16:06 +0300

    Add basic_operations.py in to python_introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5 
+sum = number1 + number2 
+print("Addition of ",number1 ,"and ",number2 ,"is",sum)
+sub = number1 - number2
+print("subtraction of ",number1 ,"and ",number2 ,"is",sub)
+multi = number1 * number2
+print("multiplication of ",number1 ,"and ",number2 ,"is",multi)
