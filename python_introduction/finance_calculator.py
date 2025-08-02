358d28ec6455139cdf8c4f5c3f2aeccbd0004930
kibrom hailemariam <kibromhailemariam21@gmail.com>
2025-08-02 11:28:21 +0300

    Fix variable name to match ALX checker requirements

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,9 @@
-monthly_income = int(input("Enter your monthly income:"))
-total_monthly_expenses = int(input("Enter your total monthly expenses:"))
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-print("Enter your monthly income:",monthly_income)
-print("Enter your total monthly expenses:",total_monthly_expenses)
-print("Your monthly savings are $",monthly_savings)
-print("Projected savings after one year, with interest, is:$",projected_savings)
+
+print("Enter your monthly income:", monthly_income)
+print("Enter your total monthly expenses:", monthly_expenses)
+print("Your monthly savings are $", monthly_savings)
+print("Projected savings after one year, with interest, is:$", projected_savings)
