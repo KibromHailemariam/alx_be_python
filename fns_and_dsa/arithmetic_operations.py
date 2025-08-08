ae130e06b92f2ae036fa5a196ff0474c9bd5263b
KibromHailemariam <kibromhailemariam21@gmail.com>
2025-08-08 09:53:18 -0700

    Fix: update perform_operation with ValueError for divide by zero and invalid operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,15 @@
-def perform_operation(num1: float,num2: float,operation: str):
+def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
-    elif operation == "division":
+    elif operation == "divide":
         if num2 != 0:
             return num1 / num2
         else:
-            return "can not divide by 0" 
-        
-    
+            return "Cannot divide by zero"
+    else:
+        return "Invalid operation"
+
